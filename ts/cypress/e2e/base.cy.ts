65a30e5473017d59de4ca4cd4bece351641044b1
Gustavo Ferreira <123216383+Gustavoferreira019@users.noreply.github.com>
2024-04-19 11:00:04 -0300

    Adicionado evento click para resolver bug

--- ts/cypress/e2e/base.cy.ts
+++ ts/cypress/e2e/base.cy.ts
@@ -9,18 +9,13 @@ describe('checa elementos básicos', () => {
   });
 
   it('botão de expandir resumo existe', () => {
-    cy.get('.btn-show-more').contains('ver mais');
+    cy.get('.btn-show-more').contains('ver mais')
+    cy.get('.btn-show-more').click();
     cy.compareSnapshot('Trabalho - Resumo expandido');
   });
 
   it('botão de criar tópico existe', () => {
     cy.get('.btn-create-topic').contains('criar tópico');
-  });
-
-  it('expandir tópico funciona', () => {
-    cy.get('.answered-topic .ops-topic-subject').click();
-    cy.get('.comments-container').should('exist');
-    cy.compareSnapshot('Trabalho - Card de topico expandido');
   });
 
   it('clicar em `criar tópico` exibe o formulário', () => {
@@ -31,12 +26,19 @@ describe('checa elementos básicos', () => {
     cy.get('input.subject').invoke('attr', 'placeholder').should('contain', 'Defina um tópico sucinto para notificar os autores...');
     cy.compareSnapshot('Trabalho - Criando novo topico');
   });
-
+  
+  
   it('enviar o formulário exibe mensagem de sucesso', () => {
     cy.get('.btn-create-topic').click();
-    cy.get('button').click();
+    cy.get('.submit-button').click();
     cy.get('body').contains('Aguardando feedback dos autores');
     cy.compareSnapshot('Trabalho - Topico enviado');
   });
 
+  it('expandir tópico funciona', () => {
+    cy.get('.answered-topic').click();
+    cy.get('.comments-container').should('exist');
+    cy.compareSnapshot('Trabalho - Card de topico expandido');
+  });
+  
 })
